c0f385ea1631ae4ec041dd8c43707ef6ed1c1b11
webvertize-ro <contact@webvertize.ro>
2025-12-30 15:49:46 +0200

    further changes

--- src/components/ConversationsList.jsx
+++ src/components/ConversationsList.jsx
@@ -1,4 +1,12 @@
 import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const Visitor = styled.div`
+  border: 1px solid black;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+`;
 
 function ConversationsList() {
   const [visitors, setVisitors] = useState(null);
@@ -7,7 +15,7 @@ function ConversationsList() {
     async function getConversations() {
       const res = await fetch('/api/getVisitors');
       const data = await res.json();
-      setVisitors(data.visitors);
+      setVisitors(data.visitors || []);
     }
     getConversations();
   }, []);
@@ -15,10 +23,16 @@ function ConversationsList() {
   return (
     <div>
       {visitors?.map((visitor) => (
-        <div>
-          <div>{visitor.name}</div>
-          <div>{visitor.phone_number}</div>
-        </div>
+        <Visitor>
+          <div>
+            <strong>Name</strong>
+            <div>{visitor.name}</div>
+          </div>
+          <div>
+            <strong>Phone Number</strong>
+            <div>{visitor.phone_number}</div>
+          </div>
+        </Visitor>
       ))}
     </div>
   );
