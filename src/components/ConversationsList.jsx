cb19141e02aec01e222da22cdf7fddfa95749c0d
webvertize-ro <contact@webvertize.ro>
2025-12-30 15:45:04 +0200

    further changes

--- src/components/ConversationsList.jsx
+++ src/components/ConversationsList.jsx
@@ -1,16 +1,27 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function ConversationsList() {
+  const [visitors, setVisitors] = useState(null);
+
   useEffect(() => {
     async function getConversations() {
       const res = await fetch('/api/getVisitors');
       const data = await res.json();
-      console.log(data);
+      setVisitors(data.visitors);
     }
     getConversations();
   }, []);
 
-  return <div></div>;
+  return (
+    <div>
+      {visitors.map((visitor) => (
+        <div>
+          <div>{visitor.name}</div>
+          <div>{visitor.phone_number}</div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default ConversationsList;
