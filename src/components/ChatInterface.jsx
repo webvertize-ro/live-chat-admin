0f704e5ab57765d56f915daca0b25f071f9218ce
webvertize-ro <contact@webvertize.ro>
2025-12-31 23:01:33 +0200

    further changes

--- src/components/ChatInterface.jsx
+++ src/components/ChatInterface.jsx
@@ -73,7 +73,7 @@ function ChatInterface({ selectedConvo }) {
       try {
         const res = await fetch(`/api/getVisitors?visitorId=${selectedConvo}`);
         const data = await res.json();
-        setVisitor(data.visitors[0] ? data.visitors[0] : null);
+        setVisitor('data.visitors is: ', data.visitors);
       } catch (error) {
         console.error(error);
       }
