e64cf4219e009b56c36e474966705343e470a131
webvertize-ro <contact@webvertize.ro>
2026-01-06 16:09:11 +0200

    further changes

--- src/components/ConversationsList.jsx
+++ src/components/ConversationsList.jsx
@@ -114,7 +114,6 @@ function ConversationsList({ onSelectedConvo }) {
             placeholder="Caută..."
             aria-label="Search"
           ></input>
-          <div>test</div>
         </form>
       </ConversationsSearchBar>
       {conversations.map((visitor, i) => (
