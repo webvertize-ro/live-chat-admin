ef624dbc7d79305baabcdbd94775354d85fbe873
webvertize-ro <contact@webvertize.ro>
2026-01-06 18:59:37 +0200

    further changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import ChatLayout from './components/ChatLayout';
 import ConversationsList from './components/ConversationsList';
 import '../App.css';
-import { Route, Router, Routes } from 'react-router';
+import { Route, BrowserRouter, Routes } from 'react-router';
 
 import Login from './pages/Login';
 
 function App() {
   return (
     <div>
-      <Router>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<ChatLayout />} />
           <Route path="/login" element={<Login />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </div>
   );
 }
