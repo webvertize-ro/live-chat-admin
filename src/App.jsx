9c115c1961733ecb68726194ffba8cc463d71222
webvertize-ro <contact@webvertize.ro>
2026-01-06 19:50:15 +0200

    implemented log in

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import ChatLayout from './components/ChatLayout';
 import ConversationsList from './components/ConversationsList';
 import '../App.css';
 import { Route, BrowserRouter, Routes } from 'react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import Login from './pages/Login';
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<ChatLayout />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={QueryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<ChatLayout />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </div>
   );
 }
